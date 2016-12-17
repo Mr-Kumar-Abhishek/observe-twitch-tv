59a45764a56c8042437096cff6818f2ff6ad8d1b
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-17 21:34:04 +0530

    automatically add grids according to the data to be shown

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ function construct_url(stream){
 function get_data(d_stream){
 	$.getJSON(construct_url(d_stream), function(data){
 		console.log(data);
+		print_html(data);
 	});
 }
 
@@ -16,6 +17,25 @@ function get_all_data(arr) {
 	arr.forEach(function(element){
 		get_data(element);
 	});
+}
+
+function print_html(json_data){
+	if(json_data.stream == null || json_data.stream == undefined){
+		print_start("offline");
+		print_end();
+	}
+	else {
+		print_start("online");
+		print_end();
+	}	
+}
+
+function print_start(status){
+	$(".show").append("<div id='#"+ status +"' class='col-xs-12 col-md-6' >");
+}
+
+function print_end(){
+	$(".show").append("</div>");
 }
 
 $(get_all_data(streams));
