b0acab7c75c7c5d1717afde60c2e845f1cd5eab6
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-17 16:51:54 +0530

    function to construct urls

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 var streams = ['freecodecamp','test_channel'];
 
 console.log(streams);
+
+function construct_url(stream){
+	var url = 'https://wind-bow.gomix.me/twitch-api/streams/' + stream + '?callback=?';
+	return url;
+}
+
+console.log(construct_url(streams[1]));
