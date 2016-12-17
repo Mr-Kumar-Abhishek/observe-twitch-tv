f362dbda8d2907aa6f93a417d5d8be2000a44a6e
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-17 17:10:15 +0530

    include function for getting data from the API

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var streams = ['freecodecamp','test_channel'];
 
-console.log(streams);
-
 function construct_url(stream){
 	var url = 'https://wind-bow.gomix.me/twitch-api/streams/' + stream + '?callback=?';
 	return url;
 }
 
-console.log(construct_url(streams[1]));
+function get_data(d_stream){
+	$.getJSON(construct_url(d_stream), function(data){
+		console.log(data);
+	});
+}
+
+$(get_data(streams[1]));
