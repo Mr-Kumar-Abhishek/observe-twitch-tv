181ff1297cd7e45bb25a7b7044250b7292a46b68
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-18 08:56:21 +0530

    include the game which is played

--- js/main.js
+++ js/main.js
@@ -21,10 +21,10 @@ function get_all_data(arr) {
 
 function print_html(json_data, d_stream){
 	if(json_data.stream == null || json_data.stream == undefined){
-		$(".show").append("<div class='row'><div id='offline' class='col-xs-6'><h4><a href='#'>" + d_stream + "</a></h1></div><div class='col-xs-6'><h4>offline</h4></div>" );
+		$(".show").append("<div class='row'><div id='offline' class='col-xs-4'><h4><a href='#'>" + d_stream + "</a></h1></div><div class='col-xs-4 col-xs-offset-4'><h4>offline</h4></div>" );
 	}
 	else {
-		$(".show").append("<div class='row'><div id='online' class='col-xs-6'><h4><a href='#'>" + d_stream + "</a></h1></div><div class='col-xs-6'><h4>online</h4></div>" );
+		$(".show").append("<div class='row'><div id='online' class='col-xs-4'><h4><a href='#'>" + d_stream + "</a></h1></div><div class='col-xs-4'><h4>" + json_data.stream.channel.game + "</h4></div><div class='col-xs-4'><h4>online</h4></div>" );
 	}
 }
 	
